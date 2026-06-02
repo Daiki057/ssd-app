2744363fcd378d374d74308acece0fdee2efa609
Daiki057 <j25032dk@stu.nagano.ac.jp>
2026-06-02 15:11:34 +0900

    STEP4  firebaseの導入

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from 'react-native';
+import '../firebaseConfig';
 
 export default function HomeScreen() {
   return (
---
+++ app/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAjFqd_234vpz3hLZnP1Q9SSMaLeGWcx_o",
+  authDomain: "ssd-project01-90c2e.firebaseapp.com",
+  projectId: "ssd-project01-90c2e",
+  storageBucket: "ssd-project01-90c2e.firebasestorage.app",
+  messagingSenderId: "614155334500",
+  appId: "1:614155334500:web:011fcef2c037f4a1e28071",
+  measurementId: "G-PWLM7TQLXY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
