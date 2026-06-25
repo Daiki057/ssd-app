236a21000573cb4af8c4681a258f7a1eed2e5bbd
Daiki057 <j25032dk@stu.nagano.ac.jp>
2026-06-25 11:59:11 +0900

    QRの読み取り(qr画面にQRと追加ボタンを表示させたい！)

---
+++ app/qrScanner.tsx
@@ -0,0 +1,119 @@
+// qrScanner.tsx のコード説明: このファイルは React コンポーネントを定義し、アプリの表示と操作を担当します。
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from "react-native";
+
+
+import {
+  CameraView,
+  useCameraPermissions
+} from "expo-camera";
+
+
+export default function QRScanner(){
+
+  const [
+    permission,
+    requestPermission
+  ] = useCameraPermissions();
+
+
+
+  if(!permission){
+
+    return <View />;
+
+  }
+
+
+
+  if(!permission.granted){
+
+    return(
+
+      <View
+        style={styles.container}
+      >
+
+        <Text>
+          カメラ権限が必要です
+        </Text>
+
+
+        <Button
+          title="許可する"
+          onPress={requestPermission}
+        />
+
+      </View>
+
+    );
+
+  }
+
+
+
+  return(
+
+    <View
+      style={styles.container}
+    >
+
+      <CameraView
+        style={styles.camera}
+        facing="back"
+        onBarcodeScanned={({data})=>{
+
+          console.log(
+            "取得uid:",
+            data
+          );
+
+        }}
+        barcodeScannerSettings={{
+          barcodeTypes:[
+            "qr"
+          ]
+        }}
+      />
+
+
+      <Text
+        style={styles.text}
+      >
+        QRを読み取ってください
+      </Text>
+
+
+    </View>
+
+  );
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+container:{
+ flex:1,
+ justifyContent:"center",
+ alignItems:"center"
+},
+
+
+camera:{
+ width:"100%",
+ height:"80%"
+},
+
+
+text:{
+ fontSize:20,
+ marginTop:20
+}
+
+});
